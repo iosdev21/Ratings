383953a14bd73899a999d0cb223726d56cff8e87
IOS Dev <ios.dev.21@outlook.com>
2017-03-12 18:01:45 -0400

    Complete Player table view.
    Complete player cell(custom cell) function.

---
+++ Ratings/Ratings/PlayerCell.swift
@@ -0,0 +1,42 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Admin on 3/12/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    var player: Player!{
+        didSet {
+            gameLabel.text = player.game
+            nameLabel.text = player.name
+            ratingImageView.image = imageForRating(rating: player.rating)
+        }
+    }
+    
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Ratings/Ratings/PlayersViewController.swift
@@ -0,0 +1,112 @@
+//
+//  PlayersViewController.swift
+//  Ratings
+//
+//  Created by Admin on 3/10/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+var players:[Player] = playersData
+
+class PlayersViewController: UITableViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return players.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath) as! PlayerCell
+
+        let player = players[indexPath.row] as Player
+        cell.player = player
+//        cell.textLabel?.text = player.name
+//        cell.detailTextLabel?.text = player.game
+//        if let nameLabel = cell.viewWithTag(100) as? UILabel{
+//            nameLabel.text = player.name
+//        }
+//        if let gameLabel = cell.viewWithTag(101) as? UILabel{
+//            gameLabel.text = player.game
+//        }
+//        if let ratingImageView = cell.viewWithTag(102) as? UIImageView{
+//            ratingImageView.image = self.imageForRating(rating: player.rating)
+//        }
+
+        return cell
+    }
+    
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
